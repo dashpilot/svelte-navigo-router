b2608f6d7cc2adaa05cd2493f06f4bdb15ec2a54
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-10 23:00:07 +0000

    test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import "./app.css";
 import App from "./App.svelte";
 
-document.getElementById("app").innerHTML = "";
+// document.getElementById("app").innerHTML = "";
 const app = new App({
   target: document.getElementById("app"),
+  hydrate: true,
 });
 
 export default app;
