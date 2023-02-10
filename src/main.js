6fcd1ac6da04f7e4647bead7cf13dbe1284b5565
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-10 22:56:25 +0000

    tweak

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./app.css";
 import App from "./App.svelte";
 
-// document.getElementById("app").innerHTML = "";
+document.getElementById("app").innerHTML = "";
 const app = new App({
   target: document.getElementById("app"),
 });
