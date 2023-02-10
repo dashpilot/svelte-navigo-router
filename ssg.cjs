c5e37efad46cc4ac22fa6e4c0163086c968f5933
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-10 22:50:17 +0000

    test statically rendering multiple pages

--- ssg.cjs
+++ ssg.cjs
@@ -3,7 +3,7 @@ require("svelte/register");
 const fs = require("fs");
 
 const Page = require("./src/App.svelte").default;
-async function getData() {
+async function renderPage() {
   const SSR = true;
 
   const res = await fetch(
@@ -12,34 +12,41 @@ async function getData() {
   var data = await res.json();
 
   data.ssr = true;
-  data.page = "about";
-  // console.log(data);
 
-  const { html, css, head } = Page.render({
-    data: data,
-  });
+  for (item of data.pages) {
+    data.page = item.slug;
+    // console.log(data);
 
-  const fullHtml = `<!DOCTYPE html>
-	<html>
-  <head>
-  <meta charset="UTF-8" />
-  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vite + Svelte</title>
-  <script type="module" crossorigin src="/assets/index.js"></script>
-  <link rel="stylesheet" href="/assets/index.css">
-  </head>
-	<body>
-  
-	  ${html}
-  
-	</body>
-	</html>
-  `;
+    const { html, css, head } = Page.render({
+      data: data,
+    });
 
-  // console.log(fullHtml);
+    const fullHtml = `<!DOCTYPE html>
+	  <html>
+    <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + Svelte</title>
+    <script type="module" crossorigin src="/assets/index.js"></script>
+    <link rel="stylesheet" href="/assets/index.css">
+    </head>
+	  <body>
+    
+	    ${html}
+    
+	  </body>
+	  </html>
+    `;
 
-  fs.mkdirSync("dist/about");
-  fs.writeFileSync("dist/about/index.html", fullHtml, "utf-8");
+    // console.log(fullHtml);
+
+    if (page == "home") {
+      fs.writeFileSync("dist/index.html", fullHtml, "utf-8");
+    } else {
+      fs.mkdirSync("dist/" + page);
+      fs.writeFileSync("dist/" + page + "/index.html", fullHtml, "utf-8");
+    }
+  }
 }
-getData();
+renderPage();
