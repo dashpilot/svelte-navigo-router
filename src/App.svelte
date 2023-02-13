c257e7b311647189d8604d03f4c9c2f7fce4e066
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-13 22:21:40 +0000

    added cache-busting

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
   
   onMount(async () => {
     var resp = await fetch(
-      "https://api.eu-central-1.linodeobjects.com/test/data.json"
+      "https://api.eu-central-1.linodeobjects.com/test/data.json", {cache: "no-store"}
     );
     data = await resp.json();
     console.log(data)
