d40791e132cb5f95ef56367aebe99e36a3c59954
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-14 11:38:23 +0000

    Added detail pages

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   export let data;
   let router;
   let entries;
+  let notfound = false;
   
   if(data && data.ssr){      
     entries = data.entries.filter(x=>x.page==data.page)
@@ -17,23 +18,36 @@
     data = await resp.json();
     console.log(data)
 
-
     router = new Navigo("/");
 
     router.on("/", async function () {
       console.log("home")
+      notfound = false;
       entries = data.entries.filter(x=>x.page=='home')
     });
-  
-    router.on("/about", async function () {
-      console.log("about")
-      entries = data.entries.filter(x=>x.page=='about')
-    });
     
-    router.on("/contact", async function () {
-      console.log("contact")
-      entries = data.entries.filter(x=>x.page=='contact')
+    data.pages.forEach(item=>{
+      router.on("/"+item.slug, async function () {
+        console.log("about")
+        notfound = false;
+        entries = data.entries.filter(x=>x.page==item.slug)
+      });
+    })
+    
+    data.entries.forEach(item=>{
+      router.on("/article/"+slugify(item.title, item.id), async function () {
+        console.log("about")
+        notfound = false;
+        entries = data.entries.filter(x=>x.id==item.id)
+      });
+    })
+    
+    router.notFound(() => {
+      // this runs if there is no match found
+      notfound = true;
     });
+  
+    
     setTimeout(()=>{
       router.resolve();
     }, 50)
@@ -47,11 +61,32 @@
         data = data;
         entries = data.entries.filter(x=>x.page==mydata.page)
       }
-  
-
     
   });
   
+  function slugify(str, id){
+    
+        if(str.length < 3){
+          str = str+"-"+id;
+        }
+  
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+      
+        // remove accents, swap ñ for n, etc
+        var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+        var to   = "aaaaeeeeiiiioooouuuunc------";
+        for (var i=0, l=from.length ; i<l ; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+    
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+    
+        return str;
+  }
+ 
 
 </script>
 
@@ -69,17 +104,21 @@
 </nav>
 
 <div class="main mt-5">
- 
   
-  
-  {#if entries}
- {#each entries as item}
- <section class="editable" id="{item.id}" data-fields="title=txt&amp;body=rte&amp;image=img&amp;delete=del">
- <h2>{item.title}</h2>
- {@html item.body}
- </section>
- {/each}
+ {#if entries}
+   {#each entries as item}
+     <section class="editable" id="{item.id}" data-fields="title=txt&amp;body=rte&amp;image=img&amp;delete=del">
+     <h2><a href="/article/{slugify(item.title, item.id)}" data-navigo>{item.title}</a></h2>
+     {@html item.body}
+     </section>
+   {/each}
  {/if}
+ 
+ {#if notfound}
+  <h2>Error 404</h2>
+  <p>Not Found</p>
+ {/if}
+ 
 </div>
 
 <style>
